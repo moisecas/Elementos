350772b9e46a6bf90c22b07df66b4f7c182c176c
Your Name <moisesjuniorcas20@gmail.com>
2023-01-24 16:57:12 -0500

    editar ok, cambio de estado ok

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -37,4 +37,24 @@ router.post('/edit/:id', async (req, res) => {
     res.redirect("/"); 
 });
 
+router.get("/delete/:id", async (req, res) => {
+    const { id } = req.params;
+    await Task.findByIdAndDelete(id);
+    res.redirect("/");
+});
+
+
+router.get('/toggleDone/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const task = await Task.findById(id);
+        await Task.findByIdAndUpdate(id, { $set: { done: task.done ? false : true } }, { new: true });
+        res.redirect('/');
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Error while updating the task');
+    }
+});
+
+
 export default router; 
